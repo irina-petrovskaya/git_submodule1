973f57c4c6e52a54e731b9d2202f7f9a9352df99
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2017-03-02 17:50:10 +0300

    add Boo4.java, Foo5.java

---
+++ src/a/b/c/Foo5.java
@@ -0,0 +1,10 @@
+package a.b.c;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 3/2/2017.
+ * Project: git_submodules_test1
+ * *******************************
+ */
+public class Foo5 {
+}
