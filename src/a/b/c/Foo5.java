211e142ea4f4ee4268e6217b2b0cb293159e04b3
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-06-08 14:01:03 +0300

    AAA1.java - main Foo5.java - submodule1 Boo0.java - submodule2

--- src/a/b/c/Foo5.java
+++ src/a/b/c/Foo5.java
@@ -4,6 +4,7 @@ package a.b.c;
  * *******************************
  * Created by Irina.Petrovskaya on 3/2/2017.
  * Project: git_submodules_test1
+ * submodule1
  * *******************************
  */
 public class Foo5 {
